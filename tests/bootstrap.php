f44a08658eadda6e563af91ccb78a41d7ec1727b
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2017-08-05 21:53:09 +0000

    set up test suite

---
+++ tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package GrottoPress\Setter
+ * @since 0.1.1
+ *
+ * @author GrottoPress (https://www.grottopress.com)
+ * @author N Atta Kus Adusei (https://twitter.com/akadusei)
+ */
+
+/**
+ * Autoloader
+ *
+ * @since 0.1.1
+ */
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
