8df25200c2c312c01bf093b538e573509ad3ec06
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2017-08-05 00:34:39 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Setter
+
+* Author: [@GrottoPress](https://github.com/grottopress)
+* Author Website: [https://www.grottopress.com](https://www.grottopress.com)
+* Contributor(s): [@akadusei](https://github.com/akadusei)
+* License: [GNU General Public License v2.0 or later](http://www.gnu.org/licenses/gpl-2.0.html)
+
+## Description
+
+*Setter* is a simple library to set/update object attributes.
+
+## Usage
+
+Install via composer:
+
+`composer require grottopress/setter`
+
+Setter is a trait, so you need to import into your class to use it:
+
+    <?php
+
+    use GrottoPress\WordPress\Setter\Setter;
+
+    class My_Class {
+        // Import setter
+        use Setter;
+
+        protected $att_1;
+        protected $att_2;
+        protected $att_3;
+
+        // Set your settable attributes here
+        protected function settables() {
+            return [ 'att_1', 'att_2' ];
+        }
+
+        // Output attributes
+        public function output( $attribute ) {
+            var_dump( $attribute );
+        }
+
+        ...
+    }
+
+    // Instantiate
+    $object = new My_Class();
+
+    // Try to set attributes
+    $object->set( 'att_1', 'World' )->output( 'att_1' ); // World
+    $object->set( 'att_2', '33', 'intval' )->output( 'att_2' ); // 33
+    $object->set( 'att_3' )->output( 'att_3' ); // Error: not settable
---
+++ src/setter.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Setter.
+ *
+ * Set attributes of an object.
+ *
+ * @package GrottoPress\Setter
+ * @since 0.1.0
+ *
+ * @author GrottoPress (https://www.grottopress.com)
+ * @author N Atta Kus Adusei (https://twitter.com/akadusei)
+ */
+
+namespace GrottoPress\Setter;
+
+if ( ! trait_exists( __NAMESPACE__ . '\Setter' ) ) :
+
+/**
+ * Setter
+ *
+ * @since 0.1.0
+ */
+trait Setter {
+    /**
+     * Get attribute
+     *
+     * @var string $attribute Name of attribute to set.
+     * @var mixed $value Value to set.
+     * @var callable $sanitize_callback Callback function to sanitize value.
+     *
+     * @since 0.1.0
+     * @access public
+     *
+     * @return mixed Attribute
+     */
+    final public function set( $attribute, $value, $sanitize_callback = '' ) {
+        if ( ! property_exists( get_called_class(), $attribute ) ) {
+            throw new Exception( "$attribute attribute does not exist." );
+        }
+
+        if ( ! in_array( $attribute, $this->settables() ) ) {
+            throw new Exception( "Setting $attribute attribute is not allowed." );
+        }
+
+        if ( ! $sanitize_callback ) {
+            $this->$attribute = $value;
+
+            return $this;
+        }
+
+        if ( ! is_callable( $sanitize_callback ) ) {
+            throw new Exception( "$sanitize_callback is not a callable." );
+        }
+
+        $this->$attribute = call_user_func( $sanitize_callback, $value );
+
+        return $this;
+    }
+
+    /**
+     * Settables
+     *
+     * Defines the attributes that can be updated
+     * with our setter.
+     *
+     * @since 0.1.0
+     * @access protected
+     *
+     * @return array Attributes.
+     */
+    protected function settables() {
+        return [];
+    }
+}
+
+endif;
